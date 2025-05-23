2a06284361bd972a807a8e457001f1d1c32a7d2a
saizversal <saikumarzversal@gmail.com>
2025-05-23 16:49:56 +0530

    Update ecs-fargate.sh

--- ecs-fargate.sh
+++ ecs-fargate.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "Starting deployment..."
-
-# Debug print to verify environment variables
+echo "Starting deployment with parameters:"
 echo "AWS_REGION=$AWS_REGION"
 echo "STACK_NAME=$STACK_NAME"
 echo "PROJECT_NAME=$PROJECT_NAME"
@@ -17,6 +15,8 @@ if [ -z "$ECR_IMAGE_URI" ]; then
   exit 1
 fi
 
+echo "Deploying CloudFormation stack..."
+
 aws cloudformation deploy \
   --template-file template.yaml \
   --stack-name "$STACK_NAME" \
@@ -26,8 +26,8 @@ aws cloudformation deploy \
     AWSRegion="$AWS_REGION" \
     AWSAccountID="$ACCOUNT_ID" \
     ProjectName="$PROJECT_NAME" \
-    ECSClusterName="$ECS_CLUSTER_NAME" \
-    ECSContainerName="$ECS_CONTAINER_NAME" \
+    ECRCluster="$ECS_CLUSTER_NAME" \
+    ECRContainer="$ECS_CONTAINER_NAME" \
     ECSContainerImageURI="$ECR_IMAGE_URI" \
     ContainerPort="$CONTAINER_PORT"
 
